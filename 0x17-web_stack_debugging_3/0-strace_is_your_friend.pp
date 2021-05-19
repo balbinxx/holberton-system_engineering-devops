5c0797a53e61d1e39b2bcbb7bfee8d6dac679dfb
balbinxx <1815@holbertonschool.com>
2021-05-19 10:54:19 -0500

    first

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using strace, find out why Apache is returning a 500 error.
+#Once you find the issue, fix it and then automate it using Puppet.
+exec { 'debugging':
+    provider => 'shell',
+    command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
+}
